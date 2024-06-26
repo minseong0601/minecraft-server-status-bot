b940725031d2b387c4cabb60c11722f4fe2ab466
minseong <54202716+minseong0601@users.noreply.github.com>
2024-06-26 22:22:51 +0900

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,51 @@
+import Discord, { Guild } from 'discord.js';
+import axios from 'axios';
+import fs from 'fs';
+
+const client = new Discord.Client({
+    intents: [ 'Guilds' ]
+});
+
+setInterval(async () => {
+    const { status, data } = await axios.get(`https://api.mcsrvstat.us/3/${JSON.parse(fs.readFileSync('./config.json', 'utf-8')).serverAddress}`);
+    
+    if ( status >= 200 && status < 300 ) {
+        const onlineCount = client.channels.cache.find(channel => channel.id === '1255455700210024468') as Discord.VoiceChannel;
+
+        data.online ? console.log(`Server is online ${data.players.online}`) : console.log("Server is offline");
+
+        if (data.online === true) {
+            try {
+                await onlineCount.setName(`접속중인 유저 ${data.players.online} 명`);
+                await client.user?.setActivity(`접속중인 유저 ${data.players.online} 명`);
+            } catch (error) {
+                console.log(error);
+            }
+        } else {
+            await onlineCount.setName(`서버 오프라인`);
+        }
+    } else {
+        console.log("Failed to get data");
+    }
+}, 30 * 1000);
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.inGuild) return
+
+    if(interaction.isChatInputCommand()) {
+        if(interaction.commandName === 'bind') {
+            const address = interaction.options.getString('address', true);
+
+            const addressVC = interaction.guild?.channels.cache.find(channel => channel.id === `1254247481521340507`) as Discord.VoiceChannel;
+
+            fs.writeFileSync(`./config.json`, JSON.stringify({ serverAddress: address }));
+
+            await interaction.guild?.channels.edit(addressVC, { name: `${address}` });
+            await interaction.reply(`server address updated to ${address}`);
+        }
+    }
+});
+
+client.on('ready', () => console.log(`${client.user?.tag} is ready!`));
+
+client.login("");
